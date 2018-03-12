6cc66d69628784aa0352166f7cb3bfb36425ad17
Lars Claussen <lars.claussen@nelen-schuurmans.nl>
2018-03-12 11:01:21 +0100

    Preparing release 0.1.2

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nens/threedigrid',
-    version='0.1.2.dev0',
+    version='0.1.2',
     zip_safe=False,
 )
