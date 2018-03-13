a9ab23f40d765b95fe221f156da194b375b5594c
larsclaussen <lars.claussen@nelen-schuurmans.nl>
2018-03-13 12:11:25 +0100

    playing with colors and options

--- threedigrid/kick.py
+++ threedigrid/kick.py
@@ -2,43 +2,30 @@
 """Console script for threedigrid."""
 
 from IPython.terminal.embed import InteractiveShellEmbed
-from traitlets.config.loader import Config
 
 import click
 from admin.gridadmin import GridH5Admin
-
-cfg = Config()
+from threedigrid.help_texts import model_overview
 
 @click.command()
 @click.option('--grid-file', prompt='Path to the admin file',
               help='Path to the admin file', type=click.Path(exists=True))
-def kick_start(grid_file):
+@click.option('--ipy/--no-ipy', default=False, help='Start an interactive ipdb session')
+def kick_start(grid_file, ipy):
     """Console script for threedigrid."""
 
     grid = GridH5Admin(grid_file)
-    banner = '''
-Starting an IPython session...\n\nA GridH5Admin instance for model {} has been created (variable named grid).
-Call dir(grid) to explore. \n\nOverview of model specifics: \n
-threedicore version: \t{}
-has 1D: \t\t{}
-has 2D: \t\t{}
-has groundwater: \t{}
-has levees: \t\t{}
-'''.format(
-        grid.model_name,
-        grid.threedicore_version,
-        grid.has_1d,
-        grid.has_2d,
-        grid.has_groundwater,
-        grid.has_levees,)
 
-    ipshell = InteractiveShellEmbed(
-        config=cfg,
-        banner1=banner,
-        exit_msg='\nLeaving IPython, quiting...\n'
-    )
-    ipshell()
+    txt = model_overview(grid_file)
+    click.secho("""\nOverview of model specifics:\n""", bold=True)
+    click.secho(txt)
 
+    if ipy:
+        info_txt = '\n\nA GridH5Admin instance for model {} has been created (variable named grid). ' \
+                   'Call dir(grid) to explore.\n'.format(grid.model_name)
+        click.secho(info_txt, bg='green', fg='black', bold=True)
+        ipshell = InteractiveShellEmbed(exit_msg='Ciao...\n')
+        ipshell()
 
 if __name__ == "__main__":
     kick_start()
