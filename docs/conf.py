b9a92bb7922d5da9b15a5fde8f794ec37f3fa7e1
larsclaussen <larsclaussen@users.noreply.github.com>
2018-03-21 13:21:30 +0100

    Lars docs (#17)
    * run sphinx build with -W flag (turn warnings into errors)
    * remove static path config

--- docs/conf.py
+++ docs/conf.py
@@ -87,6 +87,9 @@ todo_include_todos = False
 #
 html_theme = 'alabaster'
 
+# Theme options are theme-specific and customize the look and feel of a
+# theme further.  For a list of options available for each theme, see the
+# documentation.
 html_theme_options = {
     'show_powered_by': False,
     'github_user': 'nens',
@@ -96,25 +99,16 @@ html_theme_options = {
     'note_bg': '#FFF59C'
 }
 
-# Theme options are theme-specific and customize the look and feel of a
-# theme further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'Threedigriddoc'
 
 # -- Options for HTMLHelp output ---------------------------------------
 
