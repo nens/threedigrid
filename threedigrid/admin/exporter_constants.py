b8c080d1f158af74e0041d9da809c536e9f5fdd1
larsclaussen <lars.claussen@nelen-schuurmans.nl>
2018-04-06 10:22:10 +0200

    test again

--- threedigrid/admin/exporter_constants.py
+++ threedigrid/admin/exporter_constants.py
@@ -2,19 +2,19 @@
 # is a only one exporter type which depends on ogr. As ogr
 # is an optional install the constants have been moved to a
 # module of their own
-from threedigrid.geo_utils import raise_import_exception
 
 try:
     from osgeo import ogr
 except ImportError:
-    raise_import_exception('ogr')
+    pass
 
-OGR_FIELD_TYPE_MAP = {
-    'int': ogr.OFTInteger,
-    'str': ogr.OFTString,
-    'real': ogr.OFTReal,
-    'float': ogr.OFTReal,
-}
+if ogr is not None:
+    OGR_FIELD_TYPE_MAP = {
+        'int': ogr.OFTInteger,
+        'str': ogr.OFTString,
+        'real': ogr.OFTReal,
+        'float': ogr.OFTReal,
+    }
 
 SHP_DRIVER_NAME = 'ESRI Shapefile'
 GEO_PACKAGE_DRIVER_NAME = 'GPKG'
