dedf5fa12c99f8f8cc014da35f9acee7c080e6a8
larsclaussen <lars.claussen@nelen-schuurmans.nl>
2018-04-06 10:18:44 +0200

    add new const file

---
+++ threedigrid/admin/exporter_constants.py
@@ -0,0 +1,21 @@
+# constants for exporter objects.  At this point there
+# is a only one exporter type which depends on ogr. As ogr
+# is an optional install the constants have been moved to a
+# module of their own
+from threedigrid.geo_utils import raise_import_exception
+
+try:
+    from osgeo import ogr
+except ImportError:
+    raise_import_exception('ogr')
+
+OGR_FIELD_TYPE_MAP = {
+    'int': ogr.OFTInteger,
+    'str': ogr.OFTString,
+    'real': ogr.OFTReal,
+    'float': ogr.OFTReal,
+}
+
+SHP_DRIVER_NAME = 'ESRI Shapefile'
+GEO_PACKAGE_DRIVER_NAME = 'GPKG'
+
