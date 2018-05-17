497b3c809547f911ad9dec895d42b36fc188215b
Lars Claussen <lars.claussen@nelen-schuurmans.nl>
2018-05-17 09:55:39 +0200

    Back to development: 0.2.7

--- threedigrid/__init__.py
+++ threedigrid/__init__.py
@@ -11,4 +11,4 @@ except ImportError:
 
 # Threedigrid version number is automatic updated with zest.releaser
 # the version number in setup.py is updated using the find_version()
-__version__ = '0.2.6'
+__version__ = '0.2.7.dev0'
