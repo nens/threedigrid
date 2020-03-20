81c29c2d24da6aa9aa2634fbbee3e00865113422
RichardB9 <richardboon1991@gmail.com>
2020-03-20 11:00:17 +0100

    Raise AttributeError instead of logging

--- threedigrid/admin/h5py_datasource.py
+++ threedigrid/admin/h5py_datasource.py
@@ -140,11 +140,9 @@ class H5pyGroup(DataSource):
 
     def __init__(self, h5py_file, group_name, meta=None, required=False):
         if group_name not in list(h5py_file.keys()) and not required:
-            logger.info(
-                '[*] {0} not found in file {1}, not required...'.format(
-                    group_name, h5py_file)
+            raise AttributeError(
+                "{0} has no group '{1}'".format(h5py_file, group_name)
             )
-            return
 
         self.group_name = group_name
 
