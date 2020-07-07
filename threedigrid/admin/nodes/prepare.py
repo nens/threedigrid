9d2b7dfc69488c2a14668f0d3c7d93541589589a
martijn-siemerink <martijn.siemerink@nelen-schuurmans.nl>
2020-07-07 09:04:09 +0200

    [DONE] Try nodgrid from threedigrid (#103)

--- threedigrid/admin/nodes/prepare.py
+++ threedigrid/admin/nodes/prepare.py
@@ -84,14 +84,27 @@ class PrepareCells:
         node_group = h5py_file['nodes']
         lgrmin = h5py_file['meta']['lgrmin'].value
         nodk = h5py_file['grid_coordinate_attributes']['nodk'].value
+        nodm = h5py_file['grid_coordinate_attributes']['nodm'].value
+        nodn = h5py_file['grid_coordinate_attributes']['nodn'].value
+        ip = h5py_file['grid_coordinate_attributes']['ip'].value
+        jp = h5py_file['grid_coordinate_attributes']['jp'].value
+
         node_types = h5py_file['nodes']['node_type'].value
 
         pixel_width = np.zeros(nodk.shape, dtype='int')
+        pixel_coords = np.full((4, nodk.shape[0]), -9999, dtype='int')
         for node_type_subset in NODE_TYPE__IN_SUBSETS['2D_ALL']:
             mask = node_types == node_type_subset
             pixel_width[mask] = lgrmin * 2 ** (nodk[mask] - 1)
+            pixel_coords[0, mask] = ip[0, nodm[mask] - 1, nodk[mask] - 1] - 1
+            pixel_coords[1, mask] = jp[0, nodn[mask] - 1, nodk[mask] - 1] - 1
+            pixel_coords[2, mask] = ip[3, nodm[mask] - 1, nodk[mask] - 1]
+            pixel_coords[3, mask] = jp[3, nodn[mask] - 1, nodk[mask] - 1]
 
         node_group.create_dataset('pixel_width', data=pixel_width, dtype='int')
+        node_group.create_dataset(
+            'pixel_coords', data=pixel_coords, dtype='int'
+        )
 
 
 class PrepareManholes:
