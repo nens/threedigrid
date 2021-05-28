ae959daa95317481f807487ed315dda145da609e
Jelle Prins <jprins@nelen-schuurmans.nl>
2021-05-28 13:28:21 +0200

    Back to development: 1.1.3

--- threedigrid/__init__.py
+++ threedigrid/__init__.py
@@ -4,4 +4,4 @@ from __future__ import absolute_import
 
 # Threedigrid version number is automatic updated with zest.releaser
 # the version number in setup.py is updated using the find_version()
-__version__ = '1.1.2'
+__version__ = '1.1.3.dev0'
