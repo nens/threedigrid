4666ce2aa37c7026810e051442cc2292681ccd03
Martijn Siemerink <martijn.siemerink@nelen-schuurmans.nl>
2021-10-21 13:47:26 +0200

    fix test.

--- tests/test_aggregateresultadmin.py
+++ tests/test_aggregateresultadmin.py
@@ -120,7 +120,9 @@ def test_get_meta_fields_nodes(agg_gr):
         'storage_area',
         'dmax',
         'drain_level',
-        'calculation_type'
+        'calculation_type',
+        's_s1d',
+        'embedded_in'
     }
 
 
