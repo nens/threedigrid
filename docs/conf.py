d4877515bef6c1a1503eec3bbb781af3be25b8d8
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2024-02-05 11:02:42 +0100

    black conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,30 +6,30 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'threedigrid'
-copyright = '2024, Nelen & Schuurmans'
-author = 'Nelen & Schuurmans'
-release = '2.2.3'
+project = "threedigrid"
+copyright = "2024, Nelen & Schuurmans"
+author = "Nelen & Schuurmans"
+release = "2.2.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx_rtd_theme',
-    'sphinx.ext.todo', 
-#    'sphinx.ext.viewcode', 
-    'sphinx.ext.autodoc',
+    "sphinx_rtd_theme",
+    "sphinx.ext.todo",
+    #    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
 ]
 
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-root_doc = 'index'
+root_doc = "index"
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
