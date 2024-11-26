84338d47ee50cbac40b35642f6d4c09c771f6c22
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2024-11-26 14:17:07 +0100

    Back to development: 2.3.3

--- threedigrid/__init__.py
+++ threedigrid/__init__.py
@@ -1,3 +1,3 @@
 # Threedigrid version number is automatic updated with zest.releaser
 # the version number in setup.py is updated using the find_version()
-__version__ = "2.3.2"
+__version__ = "2.3.3.dev0"
